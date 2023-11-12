2edbeca5b00a0dab72e53408670126ae5b77699f
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-11-12 13:43:53 +0300

    Fix models

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_121343) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sections", force: :cascade do |t|
     t.string "title", null: false
     t.datetime "created_at", null: false
