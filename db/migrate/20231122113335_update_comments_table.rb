0f8d6cdf0fdcafcea29af140f255c7fc600aef16
Анна Петросян <apetrosia@sfedu.ru>
2023-12-04 10:56:05 +0300

    Add new reference in comments table

--- db/migrate/20231122113335_update_comments_table.rb
+++ db/migrate/20231122113335_update_comments_table.rb
@@ -1,5 +1,5 @@
 class UpdateCommentsTable < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :topic_id
+    add_reference :comments, :topic, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_12_200323) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_113335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_200323) do
     t.integer "comment_rake"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "topic_id"
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -32,11 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_200323) do
     t.datetime "updated_at", null: false
     t.index ["comment_id"], name: "index_likes_on_comment_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sections", force: :cascade do |t|
@@ -66,4 +63,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_200323) do
     t.boolean "email_conf"
   end
 
+  add_foreign_key "comments", "topics"
 end
