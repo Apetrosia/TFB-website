4f9ec74a19b46337183c43aa1fc49b24a4c5dd45
AlexanderZemlyak <zemlyaksanya@mail.ru>
2023-12-12 23:19:24 +0300

    Add avatar uploading in switch photo test

--- test/integration/personal_cabinet_usage_test.rb
+++ test/integration/personal_cabinet_usage_test.rb
@@ -15,9 +15,21 @@ class PersonalCabinetUsageTest < ActionDispatch::IntegrationTest
 
     bob.get user_path(bob.session[:user_id])
 
-    bob.patch new_photo_user_path(bob.session[:user_id]), params: {  }
-    # проверка того, что у боба правильное фото в базе ...
-    # проверка, что отображается то фото ...
+    avatar_image = fixture_file_upload(users(:bob).avatar, 'image/jpeg')
+
+    bob.patch new_photo_user_path(bob.session[:user_id]), params: { user: { avatar: avatar_image } }
+
+    bob.assert_redirected_to user_path(bob.session[:user_id])
+
+    # проверка того, что у боба есть фото в базе
+    assert_equal true, users(:bob).reload.avatar.attached?
+
+    bob.follow_redirect!
+
+    # проверка, что отображается то фото
+    assert_select "img[src*='avatar.jpg']"
+
+    assert_select "img[src*='#{users(:bob).avatar.filename}']"
   end
 
   test "change_password" do
@@ -28,8 +40,17 @@ class PersonalCabinetUsageTest < ActionDispatch::IntegrationTest
 
     bob.get email_verification_path
 
+    assert_nil users(:bob).confirmation_token
+
     bob.post email_verification_path, params: { email: users(:bob).email }
-    # проверка, что пришло письмо ...
+
+    assert_not_nil users(:bob).reload.confirmation_token
+
+    # проверка, что письмо отправлено
+    assert_emails 1
+    assert_equal [bob.email], mail.to
+    assert_equal "Подтвердите пароль", mail.subject
+    assert_match(/Подтвердите ваш пароль/, mail.body.encoded)
 
     bob.get registration_edit_path
 
