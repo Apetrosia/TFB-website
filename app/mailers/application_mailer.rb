d9ac78212721f3bb08756f2dc54f88675bf85287
Анна Петросян <apetrosia@sfedu.ru>
2023-12-24 00:21:15 +0300

    Add mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+class ApplicationMailer < ApplicationMailer
+  default from: "redbread.sup@gmail.com"
   layout "mailer"
+
+  def confirm_email
+    @user = params[:user]
+    @url  = 'https://the-first-bogatyr.onrender.com/users/' + @user.id
+    mail(to: @user.email, subject: 'Подвтердите почту')
+  end
 end
