780b93f6a790a722ad7a3465f8be47fcfecf9931
comprock15 <khoruzhaia@sfedu.ru>
2023-12-24 22:25:58 +0300

    Add password change

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -23,6 +23,15 @@ class RegistrationController < ApplicationController
   end
 
   def update
+    user = User.find_by(id :session[:user_id])
+    if user.nil?
+      redirect_to auth_path, alert: "Вы должны быть авторизованы"
+    end
+    if user.update(user_params)
+      redirect_to user_path, notice: "Пароль обновлен"
+    else
+      render :edit
+    end
   end
 
   private
