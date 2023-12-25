adcc172351e546a71a077be1c5c194c8f9f243a7
Lisa <92753486+Luizett@users.noreply.github.com>
2023-12-25 20:34:44 +0300

    fix stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,12 @@
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
 
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = true
+application.debug = false
 window.Stimulus   = application
 
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
+
 
 export { application }
